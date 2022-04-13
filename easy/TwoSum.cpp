e190181af351b27403447276f2e2bdc126ff7809
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-04-13 14:03:28 +0200

    Update TwoSum.cpp

--- easy/TwoSum.cpp
+++ easy/TwoSum.cpp
@@ -1,19 +1,16 @@
-// runtime: 5ms, faster than 97.90% of C++ online submissions for TwoSum
-// memory usage: 11.5MB, less than 15.40% of C++ online submissions for TwoSum
+// runtime: 6ms, faster than 97.86% of C++ online submissions for TwoSum
+// memory usage: 10.9MB, less than 28.86% of C++ online submissions for TwoSum
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_set<int> visited;
-        unordered_map<int, int> positions;
+        unordered_map<int, int> visited;
         
-        for (int i = 0; i < nums.size(); i++) {
-            positions.emplace(nums[i], i);
+        for (int i = 0; i < nums.size(); i++) {           
+            if (visited.find(target - nums[i]) != visited.end())
+                return {i, visited[target - nums[i]]};
             
-            if (visited.count(target - nums[i]) > 0)
-                return {i, positions[target - nums[i]]};
-            
-            visited.insert(nums[i]);
+            visited.emplace(nums[i], i);
         }
         
         return {};
