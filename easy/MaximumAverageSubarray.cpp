8eaa4ed0ee0c0ef9dfe19acd45e9688713089dd4
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-04-17 22:18:55 +0200

    Create MaximumAverageSubarray.cpp

---
+++ easy/MaximumAverageSubarray.cpp
@@ -0,0 +1,24 @@
+// runtime: 189ms, faster than 81.64% of C++ online submissions for Maximum Average Subarray I
+// memory usage: 109.7MB, less than 7.74% of C++ online submissions for Maximum Average Subarray I
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        int sum = 0;
+        
+        for (int i = 0; i < k; i++)
+            sum += nums[i];
+        
+        int maximum = sum;
+        
+        cout << maximum << endl;
+        
+        for (int i = k; i < n; i++) {
+            sum -= nums[i-k] - nums[i];
+            maximum = max(maximum, sum);
+        }
+            
+        return (double)maximum / k;
+    }
+};
